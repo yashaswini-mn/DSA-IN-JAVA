4e25d85d374ac9e9b5de891651a74ebfe2fe9e48
yashaswini-mn <yashaswnimn26@gmail.com>
2024-12-29 21:09:20 +0530

    final commit

---
+++ LinearSearch.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+public class LinearSearch {
+    public static void main(String[] args) {
+        LinearSearch lns = new LinearSearch();
+        System.out.println(lns.linear());
+
+    }
+    public int linear(){
+        int Array[]= {10, 20, 30, 40, 50, 60,70, 80, 90, 100};
+        int n = Array.length;
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the target you wanna search..!");
+        int target = sc.nextInt();
+        for(int i =0;i<=n-1;i++){
+            if(Array[i]==target){
+                return i;
+            }
+        }
+        return -1;
+    }
+}
