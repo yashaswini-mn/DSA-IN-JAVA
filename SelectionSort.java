fb940cc4f8b0c155642fac3b623739016f1d61d0
yashaswini-mn <yashaswnimn26@gmail.com>
2025-01-04 14:28:14 +0530

    final commit

---
+++ SelectionSort.java
@@ -0,0 +1,29 @@
+public class SelectionSort {
+    public static void main(String[] args) {
+        int arr[] = { 64, 11, 23, 12, 50 };
+        selectionSort(arr);
+        printArray(arr);
+    }
+
+    public static void selectionSort(int[] arr) {
+        int n = arr.length;
+        for (int i = 0; i < n - 1; i++) {
+            int minIndex = i;
+            for (int j = i + 1; j < n; j++) {
+                if (arr[j] < arr[minIndex]) {
+                    minIndex = j;
+                }
+            }
+            int temp = arr[i];
+            arr[i] = arr[minIndex];
+            arr[minIndex] = temp;
+        }
+    }
+
+    public static void printArray(int[] arr) {
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i] + " ");
+        }
+        System.out.println();
+    }
+}
